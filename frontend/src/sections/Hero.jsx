bd738210690fe6632888a0b38d8fa97c91c15a56
erick <simplexerick@gmail.com>
2025-03-03 17:36:04 +0300

    changes

--- frontend/src/sections/Hero.jsx
+++ frontend/src/sections/Hero.jsx
@@ -33,7 +33,7 @@ const Hero = () => {
         className="w-full flex justify-center items-center"
       >
         <motion.img
-          className="object-cover absolute -bottom-[35rem] z-10 rounded-[32px]"
+          className="object-cover absolute top-[80vh] z-10 rounded-[32px]"
           src="/image4.jpg"
           alt=""
           inherit={{ opacity: 0 }}
