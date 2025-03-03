9c3afaff7c172895b913a700e838652a9aec239f
erick <simplexerick@gmail.com>
2025-03-03 17:27:58 +0300

    new changes

--- frontend/src/sections/Portfolio.jsx
+++ frontend/src/sections/Portfolio.jsx
@@ -15,7 +15,7 @@ const GridGallery = () => {
   if (images.length < 7) return <p>Loading...</p>;
 
   return (
-    <section className="mt-16 relative">
+    <section className="mt-16 w-full relative">
       <h2 className="text-center text-4xl font-script mb-8">Our Portfolio</h2>
       <div className="grid-container">
         {images.map((image, index) => (
